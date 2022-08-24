3a362ae53f240879d4f43528d486a3a10e36d5f4
billhu0 <shhuluojia@163.com>
2022-08-24 12:13:18 +0800

    updated ts file

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@
 
 (function () {
 
-    // DOM elements
+    // get DOM elements
 
     // sign checkbox
-    const cb_s = document.getElementById('cb-s') as HTMLInputElement;
+    const cb_s: HTMLInputElement = document.getElementById('cb-s') as HTMLInputElement;
     // exponent checkbox
     const cb_e: HTMLInputElement[] = function () {
         let l: HTMLInputElement[] = [];
@@ -24,22 +24,23 @@
     } ();
 
     const txt_s: HTMLElement = document.getElementById('txt-s') as HTMLElement; // sign checkbox text
-    const txt_e = document.getElementById('txt-e') as HTMLElement; // exponent checkbox text
-    const txt_m = document.getElementById('txt-m') as HTMLElement; // mantissa checkbox text
+    const txt_e: HTMLElement = document.getElementById('txt-e') as HTMLElement; // exponent checkbox text
+    const txt_m: HTMLElement = document.getElementById('txt-m') as HTMLElement; // mantissa checkbox text
 
-    const mean_s = document.getElementById('mean-s') as HTMLElement; // sign meaning text
-    const mean_e = document.getElementById('mean-e') as HTMLElement; // exponent meaning text
-    const mean_m = document.getElementById('mean-m') as HTMLElement; // mantissa meaning text
+    const mean_s: HTMLElement = document.getElementById('mean-s') as HTMLElement; // sign meaning text
+    const mean_e: HTMLElement = document.getElementById('mean-e') as HTMLElement; // exponent meaning text
+    const mean_m: HTMLElement = document.getElementById('mean-m') as HTMLElement; // mantissa meaning text
 
-    const bin_inp = document.getElementById('bin-input') as HTMLInputElement; // binary input
-    const dec_inp = document.getElementById('dec-input') as HTMLInputElement; // decimal input
-    const hex_inp = document.getElementById('hex-input') as HTMLInputElement; // hexadecimal input
+    const bin_inp: HTMLInputElement = document.getElementById('bin-input') as HTMLInputElement; // binary input
+    const dec_inp: HTMLInputElement = document.getElementById('dec-input') as HTMLInputElement; // decimal input
+    const hex_inp: HTMLInputElement = document.getElementById('hex-input') as HTMLInputElement; // hexadecimal input
 
-    const bin_inp_div = document.getElementById('bin-input-div') as HTMLElement;
-    const dec_inp_div = document.getElementById('dec-input-div') as HTMLElement;
-    const hex_inp_div = document.getElementById('hex-input-div') as HTMLElement;
+    // outer 'div' around the 3 inputs which controls the input-invalid-prompt.
+    const bin_inp_div: HTMLElement = document.getElementById('bin-input-div') as HTMLElement;
+    const dec_inp_div: HTMLElement = document.getElementById('dec-input-div') as HTMLElement;
+    const hex_inp_div: HTMLElement = document.getElementById('hex-input-div') as HTMLElement;
 
-    const real_vl = document.getElementById('real-value') as HTMLElement; // real value represented
+    const real_vl: HTMLElement = document.getElementById('real-value') as HTMLElement; // real value represented
 
     // base data type holding the floating point number
     const buffer: ArrayBuffer = new ArrayBuffer(4);
@@ -90,28 +91,39 @@
 
     // update binary value input according to 'view'
     function __update_bin() {
+        // get binary value (string) from dataView
         let bin: string = view.getUint32(0).toString(2);
+        // force it to be 32-character long. Add '0' at the beginning if it is not long enough.
         while (bin.length < 32) bin = '0' + bin;
+        // update binary value, and remove invalid prompt if it is present
         bin_inp.value = bin;
         bin_inp_div.classList.remove('is-invalid');
     }
 
     // update hex value input according to 'view'
     function __update_hex() {
+        // get hexadecimal value (string) from dataView
         let hex: string = view.getUint32(0).toString(16);
+        // force it to be 8-character long. Add '0' at the beginning if it is not long enough.
         while (hex.length < 8) hex = '0' + hex;
+        // update binary value, and remove invalid prompt if it is present
         hex_inp.value = hex;
         hex_inp_div.classList.remove('is-invalid');
     }
 
     // update the real number represented according to 'view'
     function __update_real() {
+        // use 'getFloat32' to get the real represented decimal value, and update display.
+        // Note that the real represented value may not equal to the user-input decimal value.
+        // For example, because of precision loss, 0.1 is stored as 0.10000000149011612.
+        // That's why we set 'real_vl' and 'dec_input' as different things.
         real_vl.innerHTML = view.getFloat32(0).toString();
     }
 
     // When checkbox is updated, this func is called.
     // It reads checkbox status, updates the dataView, and update related elements.
     function cb_updated() {
+        // traverse all the checkboxes and get the binary value (in string of '0' and '1')
         let s: string = cb_s.checked ? '1' : '0';
         let e: string = function () {
             let tmp: string = '';
@@ -127,8 +139,9 @@
             });
             return tmp;
         } ();
-
+        // parse the value and set the dataView
         view.setUint32(0, parseInt(s + e + m, 2));
+        // update elements
         __update_cb_meaning();
         __update_bin();
         __update_hex();
@@ -137,6 +150,7 @@
 
     // when binary value is updated, this function is called
     function bin_updated() {
+        // get input string
         let s: string = bin_inp.value;
         // if regexp matches, the input is valid
         if (s.match('(?:0[bB])?[0-1]{0,32}')?.toString() == s){
@@ -147,14 +161,16 @@
             __update_hex();
             __update_real();
         }
-        // clear input decimal value
+        // clear input decimal value because it is not meaningful to update it.
         dec_inp.value = '';
         dec_inp_div.classList.remove('is-invalid');
     }
 
     // when hex value is updated, this function is called
     function hex_updated() {
+        // get input string
         let s: string = hex_inp.value;
+        // if regexp matches exactly, the input is valid
         if (s.match('(?:0[xX])?[0-9a-fA-F]{0,8}')?.toString() == s){
             view.setUint32(0, parseInt(s, 16));
 
@@ -162,6 +178,7 @@
             __update_bin();
             __update_real();
         }
+        // clear input decimal value
         dec_inp.value = '';
         dec_inp_div.classList.remove('is-invalid');
     }
@@ -172,9 +189,8 @@
         // @ts-ignore
         if (s.match('-?[0-9]*(\\.[0-9]+)?') != null && s.match('-?[0-9]*(\\.[0-9]+)?')[0].toString() === s){
         // if (s.match('-?[0-9]*(\\.[0-9]+)?')?.toString() == s){
-            // view.setUint32(0, parseFloat(s));
             view.setFloat32(0, parseFloat(s));
-
+            // update elements
             __update_cb_meaning();
             __update_bin();
             __update_hex();
@@ -183,14 +199,14 @@
     }
 
     // event listeners
-    cb_s.onchange = cb_updated;
-    cb_e.forEach(e => e.onchange = cb_updated);
-    cb_m.forEach(e => e.onchange = cb_updated);
-    bin_inp.oninput = bin_updated;
-    hex_inp.oninput = hex_updated;
-    dec_inp.oninput = dec_updated;
+    cb_s.onchange = cb_updated;  // sign checkbox
+    cb_e.forEach(e => e.onchange = cb_updated); // exponent checkbox
+    cb_m.forEach(e => e.onchange = cb_updated); // mantissa checkbox
+    bin_inp.oninput = bin_updated; // binary number input
+    dec_inp.oninput = hex_updated; // decimal number input
+    hex_inp.oninput = dec_updated; // hexadecimal number input
 
-    // init
+    // init elements.
     cb_updated();
 
 })()
